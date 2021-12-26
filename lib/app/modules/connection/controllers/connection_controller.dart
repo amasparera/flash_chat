aa3f3516b97cfa19ee5ab8337f97f83572960453
amasparerea <amasparera@gmail.com>
2021-12-26 20:19:34 +0700

    fix bug button conection

--- lib/app/modules/connection/controllers/connection_controller.dart
+++ lib/app/modules/connection/controllers/connection_controller.dart
@@ -2,10 +2,17 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
 
 class ConnectionController extends GetxController {
-  void check() {
-    Connectivity().onConnectivityChanged.listen((event) {
-      if (event != ConnectivityResult.ethernet) {
-        return Get.back();
+  Connectivity connectivity = Connectivity();
+
+  void check() async {
+    // ignore: avoid_print
+    print('object');
+    connectivity.onConnectivityChanged.listen((event) {
+      if (event == ConnectivityResult.none) {
+      } else {
+        // ignore: avoid_print
+        print('check');
+        Get.back();
       }
     });
   }
